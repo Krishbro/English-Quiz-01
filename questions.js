4df7f8c8014312f2a5b5b6f650173c84e3f1db9d
Krishbro <82700919+Krishbro@users.noreply.github.com>
2022-12-08 17:01:25 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,94 +1,114 @@
+
 // creating an array and passing the number, questions, options, and answers
 let questions = [
     {
     numb: 1,
-    question: "What does HTML stand for?",
-    answer: "Hyper Text Markup Language",
+    question: "විද්‍යුත් ධාරාවක් යනු?",
+    answer: "ඉලෙක්ට්‍රෝනවල ගලා යෑමකි",
     options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
+      "ප්‍රෝටෝනවල ගලා යෑමකි.",
+      "ඉලෙක්ට්‍රෝනවල ගලා යෑමකි",
+      "නියුට්‍රෝනවල ගලා යෑමකි",
+      "නියුක්ලියෝනවල ගලා යෑමකී"
     ]
   },
     {
     numb: 2,
-    question: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
+    question: "සිගිති පරිපත බිදිනයේ කාර්ය වන්නේ?",
+    answer: "ශෝට් වීමකදී ,අදාල පරිපත කොටස පමනක් විසංදි වීම.",
     options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet"
+      "ශෝට් වීමකදී ,සම්පූර්න සැපයුම විසංදි වීම.",
+      "භූ ගත විදුලි කාංදුවකදී,සම්පූර්න සැපයුම විසංදි වීම.",
+      "ශෝට් වීමකදී ,අදාල පරිපත කොටස පමනක් විසංදි වීම.",
+      "භූ ගත විදුලි කාංදුවකදී, සැපයුමේ කොටසක් විසංදි වීම."
     ]
   },
     {
     numb: 3,
-    question: "What does PHP stand for?",
-    answer: "Hypertext Preprocessor",
+    question: "විදුලිබල අධිකාරිය සතු උපාංගය / උපාංග වන්නේ?",
+    answer: "ඉහත සියල්ල",
     options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
+      "විදුලි මීටරය",
+      "සේවා වයරය/ සවිස් වයරය",
+      "සේවා සිදහලුව",
+      "ඉහත සියල්ල"
     ]
   },
     {
     numb: 4,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
+    question: "පරිපතයකට වෝල්ට්මීටරය හා ඇමීටරය සවි කරනු ලබන්නේ?",
+    answer: "සමාංතරගතව හා ශ්‍රේණිගතව",
     options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
+      "සමාංතරගතව",
+      "ශ්‍රේණිගතව හා සමාංතරගතව",
+      "සමාංතරගතව හා ශ්‍රේණිගතව",
+      "ශ්‍රේණිගතව"
     ]
   },
     {
     numb: 5,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
+    question: "කේබලයක ප්‍රමාණය 7/0.53mm ලෙස දක්වා ඇත.එහි දක්වා ඇත්තේ?",
+    answer: "කම්බි පොටවල් ගණන/ කම්බියේ විශ්කම්භය",
     options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
+      "කම්බි පොටවල් ගණන/ කේබලයේ විශ්කම්භය",
+      "කම්බි පොටවල් ගණන/ කේබලය අරය",
+      "කම්බි පොටවල් ගණන/ කම්බියේ විශ්කම්භය",
+      "කම්බි පොටවල් ගණන/ කම්බියේ අරය"
     ]
   },
   {
-  numb: 5,
-  question: "What does XML stand for?",
-  answer: "eXtensible Markup Language",
+  numb: 6,
+  question: "චුම්බකයක චුම්බක බල රේඛා ගමන් කරනුයේ ?",
+  answer: "උත්තර ධ්‍රැවයේ සිට දක්ෂිණ ධ්‍රැවයට",
   options: [
-    "eXtensible Markup Language",
-    "eXecutable Multiple Language",
-    "eXTra Multi-Program Language",
-    "eXamine Multiple Language"
+    "උත්තර ධ්‍රැවයේ සිට දක්ෂිණ ධ්‍රැවයට",
+    "දක්ෂිණ ධ්‍රැවයේ සිට උත්තර ධ්‍රැවයට",
+    "දක්ෂිණ ධ්‍රැවයේ සිට ඊසාන දෙසට",
+    "උත්තර ධ්‍රැවයේ සිට උත්තර ධ්‍රැවයට"
   ]
 },
 {
-numb: 5,
-question: "What does XML stand for?",
-answer: "eXtensible Markup Language",
+numb: 7,
+question: "පරිණාමකයක (ට්‍රාන්ස්ෆෝමරයක) ධාරාව මනිනු ලබන්නේ ?",
+answer: "A",
 options: [
-  "eXtensible Markup Language",
-  "eXecutable Multiple Language",
-  "eXTra Multi-Program Language",
-  "eXamine Multiple Language"
+  "KW/V",
+  "CosΘ",
+  "KVA",
+  "A"
 ]
 },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
+{
+numb: 8,
+question: "ශ්‍රී ලංකාවේ ගෘහ විදුලි උපකරණ සඳහා සම්මත සැපයුම වන්නේ ?",
+answer: "230V සහ 50 HZ",
+options: [
+  "240V සහ 60 HZ",
+  "230V සහ 60 HZ",
+  "240V සහ 50 HZ",
+  "230V සහ 50 HZ"
+]
+},
+{
+numb: 9,
+question: "විදුලි පහනක් 230V,60W ලෙස ක්‍රමාංකනය කර ඇත.විදුලි පහන හරහා ගලන ධාරාව වන්නේ?",
+answer: "ඇම්පියර් 0.26",
+options: [
+  "ඇම්පියර් 0.26",
+  "ඇම්පියර් 4",
+  "ඇම්පියර් 0.3",
+  "ඇම්පියර් 3.8"
+]
+},
+{
+numb: 10,
+question: "විදුලි පරිපථයක 220V ,75W විදුලි පහන් 10ක් සහ භාවිතයට ගන්නා විදුලි කේතලයක් 1KW ප්‍රමාණයක් ගනු ලැබුවේ නම් පරිපථය හරහා මුළු ධාරා වන්නේ?",
+answer: "ඇම්පියර 7.9",
+options: [
+  "ඇම්පියර 3.4",
+  "ඇම්පියර 7.9",
+  "ඇම්පියර 8.5",
+  "ඇම්පියර 7.5"
+]
+},
 ];
